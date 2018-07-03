dd61e668b444f1913ac439126a5fbbcf463b2722
Arthurii <trokkin@gmail.com>
2018-07-03 18:35:52 +0700

    Added documentation of UNPACK macros in common.h

--- common.h
+++ common.h
@@ -3,4 +3,17 @@ struct map_t {
 	int height, width;
 } map_t;
 
+
+/**
+ * This macros defines a new variable which allows to access map_t.buffer
+ * as a two-dimensional array with width of map_t.width.
+ * 
+ * Example:
+ * void map_foo(map_t* map_, int x, int y) {
+ * 		UNPACK(map, map_);
+ * 		if(map[x][y] == '@') {
+ * 			...
+ * 		}
+ * }
+*/
 #define UNPACK(varname, map_ptr) char (*varname)[(map_ptr)->width] = (char (*)[(map_ptr)->width]) map_ptr->buffer 
