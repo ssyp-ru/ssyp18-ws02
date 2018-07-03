508381ee3518059b6f348bdc62eae952a95f9938
S1lur <kma03.12.2002@gmail.com>
2018-07-03 17:57:14 +0700

    Fixed lines' length and code styling

--- common.h
+++ common.h
@@ -5,4 +5,4 @@ typedef struct map_t {
 	int height, width;
 } map_t;
 
-#define UNPACK(varname, map_ptr) char (*varname)[(map_ptr)->width] = (char (*)[(map_ptr)->width]) map_ptr->buffer 
+#define UNPACK(varname, map_ptr) char (*varname)[(map_ptr)->width] = (char (*)[(map_ptr)->width]) map_ptr->buffer
--- mapgen.h
+++ mapgen.h
@@ -1,10 +1,12 @@
 #pragma once
 
+
 #include <stdbool.h>
 #include "common.h"
+
 
 void buildWall(map_t* map, int width, int height, bool isHorizontal, int x, int y);
 
 
-void drawMap(map_t* map); 
+void drawMap(map_t* map);
 
