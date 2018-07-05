f26aeae6537ae75dab98a0f377bf6840b53d4181
Dmitry Gorbunov <dmitry.e.gorbunov@gmail.com>
2018-07-06 05:07:07 +0700

    Replaced actor definition with Michail's one

--- common.h
+++ common.h
@@ -37,30 +37,3 @@ typedef struct map{
 	int height, width;
 } map_t;
 
-typedef struct inventory{
-	char* name;
-	unsigned int item_count;	
-}inv_t;
-
-typedef struct actor{
-	unsigned char health;
-	unsigned char speed;
-	unsigned char painchance; //if the bullet hits. Actor with any probability runs away (painchance < 256)
-	chtype view; 
-	unsigned int x;
-	unsigned int y;
-	inv_t* actor_inventory;
-	unsigned char state;	//current state.For example monster search monster
-	int flags;						//features of actor(maybe check actor is player)
-	unsigned int targx;		//-|____position of target of this actor(player, item, other actor)
-	unsigned int targy;		//-|
-}actor_t;
-
-typedef struct actors_vect{
-	actor_t* allActors;
-	unsigned int len;
-	unsigned int capacity;	
-} avect_t;
-
-
-
