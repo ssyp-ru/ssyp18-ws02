3e7a97f594eea7887252adb0d90aa79203ccec57
Ilya <strelnikovila@gmail.com>
2018-07-07 10:42:37 +0700

    Some valuable changes

--- common.h
+++ common.h
@@ -5,8 +5,13 @@
 #define FLAG_TRANS 0x2
 #define FLAG_DESTR 0x4
 
+typedef enum feature_type {
+	NOTHING, DROPPED_ITEM, CONTAINER,
+	DOOR, DEAD_BADGER
+} ftype_t;
+
 typedef struct tile{
-	chtype s;
+	chtype symbol;
   int flags;
 } tile_t;
 
@@ -16,6 +21,24 @@ typedef struct map{
  	int width;
 } map_t;	
 
+typedef struct feature{
+	inventory_t * inventory;
+	ftype_t type;
+	int flags;
+	char * description;
+	chtype symbol;
+} feature_t;
+
+typedef struct level_t {
+	map_t * map;
+	kdtree_t * features;
+} level_t;
+
+// Function for picking up items from a feature
+void pickUp(actor_t * actor, feature_t feature);
+
+//Function for dropping item on a floor
+void throwAway(item_t * item, level_t * level, int xPos, int yPos);
 
 #define UNPACK(varname, map_ptr) tile_t (*varname)[(map_ptr)->width] = (tile_t (*)[(map_ptr)->width]) map_ptr->buffer 
 
--- hero.h
+++ hero.h
@@ -1,23 +1,32 @@
 #pragma once
 #include <ncurses.h>
-
-typedef struct vision{
-	int xPos;
-	int yPos;
-} vision_t;
-typedef struct inventory{
+#define FLAG_FRIENDLY 0x1
+#define FLAG_STACKABLE 0x1
+typedef struct item {
 	chtype type;
 	float weight;
+	int flags;
 	char * description;
 	int quality;
-}inventory_t; 
+} item_t;
+
+typedef struct inventory{
+	item_t * item;
+	int amount;
+	int maxAmount;
+} inventory_t;
+
 typedef struct hero{
-	vision_t * view;
-	inventory_t * inventory;
+	inventory_t inventory;
+	int flags;
 	int hp;
+	int strength;
+	int agility;
+	int stamina;
 	int viewRad;
 	int xPos;
 	int yPos;
+//	void (*behave)() = &behave;
 	chtype look;
 } hero_t;	
 
